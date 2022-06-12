a70f6872c90f14a2524eb340c89a5e4a5ad75ffc
benjaminrike1 <benjaminrike1@gmail.com>
2022-06-12 16:24:55 +0200

    fixed cifarnet

--- models.py
+++ models.py
@@ -54,19 +54,19 @@ class LogisticRegression(nn.Module):
 
 class CifarNet(nn.Module):
   def __init__(self):
-      super(ConvNet, self).__init__()
-      self.conv1 = nn.Conv2d(3, 32, 3)
-      self.conv2 = nn.Conv2d(32, 32, 3)
+      super(CifarNet, self).__init__()
+      self.conv1 = nn.Conv2d(3, 64, 3, padding=1)
+      self.conv2 = nn.Conv2d(64, 64, 3,  padding=1)
       self.pool1 = nn.MaxPool2d(2, 2)
-      self.conv3 = nn.Conv2d(32, 64, 3)
-      self.conv4 = nn.Conv2d(64, 64, 3)
+      self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+      self.conv4 = nn.Conv2d(128, 128, 3, padding=1)
       self.pool2 = nn.MaxPool2d(2, 2)
-      self.conv5 = nn.Conv2d(64, 128, 3)
-      self.conv6 = nn.Conv2d(128, 128, 3)
+      self.conv5 = nn.Conv2d(128, 256, 3, padding=1)
+      self.conv6 = nn.Conv2d(256, 256, 3, padding=1)
       self.pool3 = nn.MaxPool2d(2, 2)
       self.flatten = nn.Flatten()
-      self.fc1 = nn.Linear(1024, 1024)
-      self.fc2 = nn.Linear(1024, 10)
+      self.fc1 = nn.Linear(4096, 4096)
+      self.fc2 = nn.Linear(4096, 10)
       self.activation = nn.ReLU()
 
   def forward(self, x):
